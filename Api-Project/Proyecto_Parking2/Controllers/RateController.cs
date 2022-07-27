41d4e48f9fa29b2fd792da0cea49d23a073ab5b1
Jesus <jesusvase@hotmail.com>
2022-07-27 17:33:03 -0600

    API
    .

---
+++ Api-Project/Proyecto_Parking2/Controllers/RateController.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Proyecto_Parking2.Models;
+
+namespace Proyecto_Parking2.Controllers
+{
+
+
+    [Route("api/[controller]")]
+    [EnableCors("GetAllPolicy")]
+    [ApiController]
+    public class RateController : Controller
+    {
+        private readonly IF4101_Project_B95571_B98295_B70274_B97291Context _context;
+
+        public RateController()
+        {
+            _context = new IF4101_Project_B95571_B98295_B70274_B97291Context();
+        }
+
+        // GET: api/Rates
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Rate>>> GetRates()
+        {
+            if (_context.Rates == null)
+            {
+                return NotFound();
+            }
+            return await _context.Rates.ToListAsync();
+        }
+
+        // GET: api/Rates/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Rate>> GetRate(int id)
+        {
+            if (_context.Rates == null)
+            {
+                return NotFound();
+            }
+            var rate = await _context.Rates.FindAsync(id);
+
+            if (rate == null)
+            {
+                return NotFound();
+            }
+
+            return rate;
+        }
+
+        // PUT: api/Rates/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRate(int id, Rate rate)
+        {
+            if (id != rate.IdRate)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(rate).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RateExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Rates
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Rate>> PostRate(Rate rate)
+        {
+            if (_context.Rates == null)
+            {
+                return Problem("Entity set 'IF4101_B91472_B92299Context.Rates'  is null.");
+            }
+            _context.Rates.Add(rate);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetRate", new { id = rate.IdRate }, rate);
+        }
+
+        // DELETE: api/Rates/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRate(int id)
+        {
+            if (_context.Rates == null)
+            {
+                return NotFound();
+            }
+            var rate = await _context.Rates.FindAsync(id);
+            if (rate == null)
+            {
+                return NotFound();
+            }
+
+            _context.Rates.Remove(rate);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+        private bool RateExists(int id)
+        {
+            return (_context.Rates?.Any(e => e.IdRate == id)).GetValueOrDefault();
+        }
+
+    }
+}
---
+++ Api-Project/Proyecto_Parking2/Controllers/SpaceController.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Proyecto_Parking2.Models;
+
+namespace Proyecto_Parking2.Controllers
+{
+
+    [Route("api/[controller]")]
+    [EnableCors("GetAllPolicy")]
+    [ApiController]
+    public class SpaceController : Controller
+    {
+
+        private readonly IF4101_Project_B95571_B98295_B70274_B97291Context _context;
+
+        public SpaceController()
+        {
+            _context = new IF4101_Project_B95571_B98295_B70274_B97291Context();
+        }
+
+        // GET: api/Spots
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Space>>> GetSpaces()
+        {
+            return await _context.Spaces.Include(s => s.IdParking).Include(s => s.IdParking).Include(s => s.IdRate).Select(spaceItem => new Space()
+            {
+                IdSpace = spaceItem.IdSpace,
+                Number = spaceItem.Number,
+                IdType = spaceItem.IdType,
+                Status = spaceItem.Status,
+                IdVehicule = spaceItem.IdVehicule,
+                IdParking = spaceItem.IdParking,
+                CheckIn = spaceItem.CheckIn,
+                IdRate = spaceItem.IdRate
+
+            }).ToListAsync();
+        }
+
+        // GET: api/Spots/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Space>> GetSpace(int id)
+        {
+            if (_context.Spaces == null)
+            {
+                return NotFound();
+            }
+            var spot = await _context.Spaces.FindAsync(id);
+
+            if (spot == null)
+            {
+                return NotFound();
+            }
+
+            return spot;
+        }
+
+        // PUT: api/Spots/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSpace(int id, Space space)
+        {
+            if (id != space.IdSpace)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(space).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SpotExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Spots
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Space>> PostSpace(Space space)
+        {
+            if (_context.Spaces == null)
+            {
+                return Problem("Entity set 'IF4101_B91472_B92299Context.Spots'  is null.");
+            }
+            _context.Spaces.Add(space);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSpot", new { id = space.IdSpace }, space);
+        }
+
+        // DELETE: api/Spots/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSpace(int id)
+        {
+            if (_context.Spaces == null)
+            {
+                return NotFound();
+            }
+            var spot = await _context.Spaces.FindAsync(id);
+            if (spot == null)
+            {
+                return NotFound();
+            }
+
+            _context.Spaces.Remove(spot);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SpotExists(int id)
+        {
+            return (_context.Spaces?.Any(e => e.IdSpace == id)).GetValueOrDefault();
+        }
+
+    }
+}
